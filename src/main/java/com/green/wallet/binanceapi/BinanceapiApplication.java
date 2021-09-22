e1978640fa3cf0c14cd51b636cd11936dd223eb1
Faran <farantariq24@gmail.com>
2021-09-22 16:44:08 +0500

    Initial commit

---
+++ src/main/java/com/green/wallet/binanceapi/BinanceapiApplication.java
@@ -0,0 +1,13 @@
+package com.green.wallet.binanceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinanceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinanceapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/green/wallet/binanceapi/BinanceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.wallet.binanceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
